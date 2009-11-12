ffcdbf62c0152802525c3cdf680b9d7be245bdf9
josh@joshbutts.com <josh@Deeporange.local>
2009-11-11 22:42:14 -0600

    fleshed out object and mapper some more

--- Modela/Mapper.php
+++ Modela/Mapper.php
@@ -1,5 +1,30 @@
 <?php
 class Modela_Mapper
 {
+	protected static $_db_adapter;
+	protected static $_cache_adapter;
 	
+	protected $_table;
+	
+	public function __construct()
+	{
+		
+	}
+	
+	public static function setDbAdapter(Zend_Db_Adapter_Abstract $adapter)
+	{
+		if (null !== $adapter) {
+			self::$_db_adapter = $adapter;
+		}
+	}
+	
+	public function save($table, $values, $overwrite = false)
+	{
+		$sql = "INSER";
+	}
+	
+	protected function _query($sql, $params)
+	{
+		$adapter = self::$_db_adapter;
+	}
 }
--- Modela/Object.php
+++ Modela/Object.php
@@ -1,5 +1,46 @@
 <?php
-class Modela_Object
+abstract class Modela_Object
 {
+	protected $_storage = array();
+	protected $_mapper;
+	protected $_table;
 	
+	public function __construct($properties = null)
+	{
+		if (is_array($properties)) {
+			foreach ($properties as $key=>$value) {
+				$this->$key = $value;
+			}
+		}
+
+		if (isset($this->_mapper_class)) {
+			
+		} else if (false) {
+			
+		} else {
+			$this->_mapper = new Modela_Mapper();
+		}
+		
+		
+		return $this;
+	}
+	
+	public function save()
+	{
+		if (!$this->_table) {
+			throw new Exception();
+		}
+		
+		$this->_mapper->save($this->_table, $this->_storage);
+	}
+	
+	public function __get($property)
+	{
+		return $this->_storage[$property];
+	}
+	
+	public function __set($property, $value)
+	{
+		$this->_storage[$property] = $value;
+	}
 }
